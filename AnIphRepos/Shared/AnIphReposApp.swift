f76f9d58d37afdf552e77d88053d4af32189d12d
Florian <>
2021-10-11 09:22:35 +0200

    Projekt angelegt

---
+++ AnIphRepos/Shared/AnIphReposApp.swift
@@ -0,0 +1,18 @@
+//
+//  AnIphReposApp.swift
+//  Shared
+//
+//  Created by Florian Panten / BBS2H20A BBM3H18M on 11.10.21.
+//
+//
+
+import SwiftUI
+
+@main
+struct AnIphReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnIphRepos/Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Florian Panten / BBS2H20A BBM3H18M on 11.10.21.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
